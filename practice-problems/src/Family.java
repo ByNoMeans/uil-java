b083a659aa1e39bb13cc94082a55e0134db5e586
sidnb13 <62273881+sidnb13@users.noreply.github.com>
2020-07-21 21:30:52 -0500

    Added PhoneNumbers.java + Qualified.java

--- practice-problems/src/Family.java
+++ practice-problems/src/Family.java
@@ -33,8 +33,8 @@ public class Family {
 
         List<Integer> values = new ArrayList<>();
         map.values().stream().distinct().forEach(values::add);
-
         Collections.sort(values);
+
         for (int v : values) {
             StringBuilder genString = new StringBuilder();
             List<String> kList = new ArrayList<>();
